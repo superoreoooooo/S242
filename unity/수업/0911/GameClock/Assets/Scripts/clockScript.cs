6bb310ea3e344db1d46ebe82ef61f8d27bdaaa5f
oreo <oreo@sch.ac.kr>
2024-09-19 13:59:25 +0900

    add gameclock

---
+++ unity/수업/0911/GameClock/Assets/Scripts/clockScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class clockScript : MonoBehaviour
+{
+    bool btn_active;
+    public TextMeshProUGUI text_time;
+    public TextMeshProUGUI btn_text;
+
+    public float startTime;
+    private float timeRemaining;
+
+
+    void Start()
+    {
+        btn_active = false;
+        Debug.Log("START" + btn_active);
+    }
+
+    public void btn_Click()
+    {
+        if (!btn_active)
+        {
+            timeRemaining = startTime;
+            setTimerOn();
+            btn_text.text = "STOP";
+        }
+        else
+        {
+            setTimerOff();
+            btn_text.text = "START";
+        }
+    }
+
+    public void setTimerOff()
+    {
+        btn_active = false;
+    }
+
+    public void setTimerOn()
+    {
+        btn_active = true;
+    }
+
+    void Update()
+    {
+        if (btn_active)
+        {
+            doCountDown();
+        }
+    }
+
+    private void doCountDown()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining < 0)
+        {
+            timeRemaining = 0;
+            btn_active = false;
+            timesUp();
+        }
+        text_time.text = timeRemaining.ToString();
+
+        Debug.Log("TIME REMAINING : " + timeRemaining);
+    }
+
+    void timesUp()
+    {
+        Debug.Log("TIME'S UP!");
+    }
+}
